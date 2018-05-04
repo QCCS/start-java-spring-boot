85c3d839437956522ba12c3fc9b958be3ab63d56
周立 <li.zhou@huilianyi.com>
2018-05-04 09:14:07 +0800

    线程(深入学习)

--- src/main/java/com/example/demo1/learn/thread/deep/deep01/MovieTicket.java
+++ src/main/java/com/example/demo1/learn/thread/deep/deep01/MovieTicket.java
@@ -5,52 +5,53 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class MovieTicket implements Runnable {
 
-	private int tickets = 100;
-	private Object obj = new Object();
+    private int tickets = 100;
+    private Object obj = new Object();
 
-	// 造锁
-	// private ReentrantLock lock = new ReentrantLock();
-	private Lock lock = new ReentrantLock();
+    // 造锁
+    // private ReentrantLock lock = new ReentrantLock();
+    private Lock lock = new ReentrantLock();
 
-	// @Override
-	// public void run() {
-	// while (true) {
-	// synchronized (obj) {
-	// if (tickets > 0) {
-	// try {
-	// Thread.sleep(100);
-	// } catch (InterruptedException e) {
-	// e.printStackTrace();
-	// }
-	//
-	// System.out.println(Thread.currentThread().getName()
-	// + "正在出售第" + (tickets--) + "张票");
-	// }
-	// }
-	// }
-	// }
+/*
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (obj) {
+                if (tickets > 0) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
-	@Override
-	public void run() {
-		while (true) {
-			try {
-				// 加锁
-				lock.lock();
+                    System.out.println(Thread.currentThread().getName()
+                            + "正在出售第" + (tickets--) + "张票");
+                }
+            }
+        }
+    }
+*/
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                // 加锁
+                lock.lock();
 
-				if (tickets > 0) {
-					try {
-						Thread.sleep(100);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
+                if (tickets > 0) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
-					System.out.println(Thread.currentThread().getName()
-							+ "正在出售第" + (tickets--) + "张票");
-				}
-			} finally {
-				// 释放锁
-				lock.unlock();
-			}
-		}
-	}
+                    System.out.println(Thread.currentThread().getName()
+                            + "正在出售第" + (tickets--) + "张票");
+                }
+            } finally {
+                // 释放锁
+                lock.unlock();
+            }
+        }
+    }
 }
