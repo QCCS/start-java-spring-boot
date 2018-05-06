a7625c359880f10a0aa5f85fb40aeff90bffb4f2
周立 <li.zhou@huilianyi.com>
2018-05-06 11:08:22 +0800

    获取路由参数与配置参数

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -1,17 +1,41 @@
 package com.example.demo1;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by zhouli on 18/5/1.
  */
 @RestController
 public class HelloController {
+
+    //可以从配置文件获取值
+    @Value("${age}")
+    private String age;
+
     @RequestMapping(value = "/api", method = RequestMethod.GET)
     public String any() {
         return "test first api";
     }
+
+    @RequestMapping(value = "/name", method = RequestMethod.GET)
+    public String getUsername() {
+        return age;
+    }
+
+
+    //获取路由的参数
+    //http://localhost:1111/test/10/name
+    @RequestMapping(value = "/{id}/name", method = RequestMethod.GET)
+    public String getId(@PathVariable("id") Integer id) {
+        return "id="+id;
+    }
+
+    //获取路由的请求参数
+    //http://localhost:1111/test/getparam?id=11
+    @RequestMapping(value = "/getparam", method = RequestMethod.GET)
+    public String getParam(@RequestParam("id") Integer id) {
+        return "paramId="+id;
+    }
+
 }
