9bd01074ee7555b91da073ebbe3192d44655a6f4
周立 <li.zhou@huilianyi.com>
2018-05-06 17:20:07 +0800

    整合MyBaits

---
+++ src/main/java/com/example/demo1/database/dao/dao.md
@@ -0,0 +1,13 @@
+#DAO (Data Access Object）
+由于添加了双数据源
+
+有了如下的配置
+```$xslt
+@EnableJpaRepositories(
+        entityManagerFactoryRef="entityManagerFactorySecondary",
+        transactionManagerRef="transactionManagerSecondary",
+        basePackages= { "com.example.demo1.domain.s" }) //设置Repository所在位置
+```
+就把dao类全部放在了对于的目录
+
+不然程序找不到就报错
--- src/test/java/com/example/demo1/GirlServiceTest.java
+++ src/test/java/com/example/demo1/GirlServiceTest.java
@@ -1,13 +1,23 @@
 package com.example.demo1;
 
-import com.example.demo1.domain.s.Girl;
-import com.example.demo1.database.service.GirlService;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+        import com.example.demo1.domain.p.User;
+        import com.example.demo1.domain.p.User1;
+        import com.example.demo1.domain.p.UserMapper;
+        import com.example.demo1.domain.s.Girl;
+        import com.example.demo1.database.service.GirlService;
+        import org.junit.Assert;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.boot.test.context.SpringBootTest;
+        import org.springframework.test.annotation.Rollback;
+        import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+        import org.springframework.test.context.junit4.SpringRunner;
+        import org.springframework.transaction.annotation.Transactional;
+
+        import java.util.HashMap;
+        import java.util.List;
+        import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,7 +29,48 @@ public class GirlServiceTest {
     @Test
     public void findOneTest() {
         //接口service：从数据库取出id是22的人，比较年纪
-        Girl girl = girlService.findOne(22);
+        //由于配置了多数源，要分享获取的哪一个数据库
+        Girl girl = girlService.findOne(1);
         Assert.assertEquals(new Integer(112), girl.getAge());
     }
+
+    //主要是测试MyBaits
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    @Rollback
+    public void testUserMapper() throws Exception {
+        // insert一条数据，并select出来验证
+        userMapper.insert("AAA", 20);
+        User u = userMapper.findByName("1");
+        Assert.assertEquals(20, u.getAge().intValue());
+        // update一条数据，并select出来验证
+        u.setAge(30);
+        userMapper.update(u);
+        u = userMapper.findByName("AAA");
+        Assert.assertEquals(30, u.getAge().intValue());
+        // 删除这条数据，并select验证
+        userMapper.delete(u.getId());
+        u = userMapper.findByName("AAA");
+        Assert.assertEquals(null, u);
+
+        u = new User("BBB", 30);
+        userMapper.insertByUser(u);
+        Assert.assertEquals(30, userMapper.findByName("BBB").getAge().intValue());
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "CCC");
+        map.put("age", 40);
+        userMapper.insertByMap(map);
+        Assert.assertEquals(40, userMapper.findByName("CCC").getAge().intValue());
+
+        List<User> userList = userMapper.findAll();
+        for(User user : userList) {
+            Assert.assertEquals(null, user.getId());
+            Assert.assertNotEquals(null, user.getName());
+        }
+
+    }
+
 }
