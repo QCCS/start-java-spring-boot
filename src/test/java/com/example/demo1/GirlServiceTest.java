c2a6ee6ba2f386adbef3167e47e1ca9bc38c522f
周立 <li.zhou@huilianyi.com>
2018-05-06 11:37:10 +0800

    添加测试

---
+++ src/test/java/com/example/demo1/GirlServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.demo1;
+
+import com.example.demo1.database.model.Girl;
+import com.example.demo1.database.service.GirlService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GirlServiceTest {
+
+    @Autowired
+    private GirlService girlService;
+
+    @Test
+    public void findOneTest() {
+        Girl girl = girlService.findOne(73);
+        Assert.assertEquals(new Integer(13), girl.getAge());
+    }
+}
